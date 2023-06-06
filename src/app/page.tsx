1e8ada5e92f596d300c0f0d2f923b9c06f33bbe7
Eduardo <orthmann2811@gmail.com>
2023-06-06 16:02:31 -0300

    add first-section html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from '@mui/material';
 export default function Home() {
   return (
     <div className="home">
-      <main>
+      <main className="main-section container">
         <h1>Olá! Seja bem vindo(a)!</h1>
         <p>
           Esta é uma rede social dedicada a amantes da natureza que querem fazer a diferença no mundo. Você é um deles?
@@ -15,6 +15,20 @@ export default function Home() {
           <Button variant="outlined">Cadastrar</Button>
         </div>
       </main>
+      <section className="first-seciton container">
+        <div className="solid-color-background">
+          <h2>Junte se a nós!</h2>
+          <div>
+            <p>
+              faça a diferença para o nosso planeta Terra. Aqui, você encontrará um espaço dedicado a promover ações
+              positivas em prol da natureza, compartilhar informações relevantes e engajar-se em atividades que ajudarão
+              a preservar o meio ambiente.
+            </p>
+            <p>Registre-se agora e seja parte da mudança!</p>
+          </div>
+          <Button variant="text">Ver mais</Button>
+        </div>
+      </section>
     </div>
   );
 }
