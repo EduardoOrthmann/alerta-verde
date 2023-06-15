b31d702f277872b63d4ad8e57e7a99e86c02a9ef
Eduardo <orthmann2811@gmail.com>
2023-06-15 20:25:40 -0300

    add theme switcher button to MUIwrapper

--- src/components/MUI-wrapper/MUIwrapper.tsx
+++ src/components/MUI-wrapper/MUIwrapper.tsx
@@ -1,63 +1,83 @@
 'use client';
 
-import React from 'react';
+import React, { ReactNode, useMemo, useState } from 'react';
 
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, IconButton, ThemeProvider, createTheme } from '@mui/material';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Plus Jakarta Sans, Arial, sans-serif',
-    fontSize: 16,
-    button: {
-      textTransform: 'capitalize',
-    },
-  },
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#04D361',
-    },
-    secondary: {
-      main: '#dbf301',
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: '10px',
-        },
-      },
-      defaultProps: {
-        size: 'large',
-      },
-    },
-    MuiAppBar: {
-      styleOverrides: {
-        colorTransparent: {
-          backdropFilter: 'blur(4px)',
-        },
-      },
-      defaultProps: {
-        elevation: 0,
-      },
-    },
-    MuiTextField: {
-      defaultProps: {
-        InputLabelProps: {
-          sx: {
-            top: '-2px',
+interface MUIwrapperProps {
+  children: ReactNode;
+}
+
+export default function MUIwrapper({ children }: MUIwrapperProps) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        typography: {
+          fontFamily: 'Plus Jakarta Sans, Arial, sans-serif',
+          fontSize: 16,
+          button: {
+            textTransform: 'capitalize',
           },
         },
-      },
-    },
-  },
-});
+        palette: {
+          mode: isDarkMode ? 'dark' : 'light',
+          primary: {
+            main: '#04D361',
+          },
+          secondary: {
+            main: '#dbf301',
+          },
+        },
+        components: {
+          MuiButton: {
+            styleOverrides: {
+              root: {
+                borderRadius: '10px',
+              },
+            },
+            defaultProps: {
+              size: 'large',
+            },
+          },
+          MuiAppBar: {
+            styleOverrides: {
+              colorTransparent: {
+                backdropFilter: 'blur(4px)',
+              },
+            },
+            defaultProps: {
+              elevation: 0,
+            },
+          },
+          MuiTextField: {
+            defaultProps: {
+              InputLabelProps: {
+                sx: {
+                  top: '-2px',
+                },
+              },
+            },
+          },
+        },
+      }),
+    [isDarkMode]
+  );
 
-export default function MUIwrapper({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline>{children}</CssBaseline>
+      <CssBaseline>
+        <IconButton
+          style={{ position: 'fixed', top: '7rem', right: '3rem' }}
+          onClick={() => setIsDarkMode(!isDarkMode)}
+        >
+          {theme.palette.mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
+        </IconButton>
+        {children}
+      </CssBaseline>
     </ThemeProvider>
   );
 }
